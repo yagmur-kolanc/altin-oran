a4af7fa11c856be963bb72d7bbdb77746898dd4f
yagmur-kolanc <65070514+yagmur-kolanc@users.noreply.github.com>
2020-06-30 22:07:17 +0300

    yeni bir dosya oluşturdum

---
+++ altin oran/altın oran.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+   int main(){
+   	 int z;
+   	double altin=1.61,sonuc,y,x;
+   	 y=(2*z)/1+pow(5,(1/2)); 
+   	 x=z-y;
+   	 sonuc=altin/y;
+   	  printf("dogru parcasýnýn uzunlugunu giriniz:\n");
+   	 scanf("%d",&z);
+   	 
+   	 printf("%d %d",&y,&x);
+   	 return 0;
+   }
