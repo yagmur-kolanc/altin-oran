6f7181bbaa51b2d07d593af15cd8b02dd6d3d7f7
yagmur-kolanc <65070514+yagmur-kolanc@users.noreply.github.com>
2020-07-05 00:18:27 +0300

    Update altın oran.c

--- altin oran/altın oran.c
+++ altin oran/altın oran.c
@@ -5,12 +5,11 @@
    	 int AB;
 	 printf("dogru parcasının uzunlugunu giriniz:\n");
    	 scanf("%d",&AB);
+	   
    	 BC=2*AB/(1+5^(1/2)); 
    	 AC=AB-BC;
    	 
    	 if((BC/AC)==(AB/BC)){
-		
-   	 
    	 printf("%f %f",&AC,&BC);
    }
    else
